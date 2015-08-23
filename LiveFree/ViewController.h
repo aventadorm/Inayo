5fecd4222af1de057f6914b227f6924d6853dba2
aventadorm <mayur.phadte@gmail.com>
2015-08-23 19:46:45 +0530

    Initial Commit

---
+++ LiveFree/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiveFree
+//
+//  Created by Mayur Phadte on 23/08/15.
+//  Copyright (c) 2015 Mayur Phadte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LiveFree/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LiveFree
+//
+//  Created by Mayur Phadte on 23/08/15.
+//  Copyright (c) 2015 Mayur Phadte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
