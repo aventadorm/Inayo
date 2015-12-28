8a62967908d27b8df10cfa601a051483653ff787
aventadorm <mayur.phadte@gmail.com>
2015-12-28 22:33:22 +0530

    Revert "Merge branch 'location-view-controller-ui'"
    This reverts commit f2d881ec00d382b5876132df0d9f496a39304969, reversing
    changes made to e7d7099e292e03a1e920893475f850327dc0b75c.

--- LiveFree/locationViewController.h
+++
@@ -1,9 +0,0 @@
-//
-//  locationViewController.h
-//  LiveFree
-//
-//  Created by utkarsh agrawal on 16/12/15.
-//  Copyright © 2015 Mayur Phadte. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
